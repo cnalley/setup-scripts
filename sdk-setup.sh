584be9e0bbd948c816cfd9305ae4f6d44dedb0d3
Casey Nalley <cnalley@kubos.co>
2016-10-02 22:17:52 -0500

    Add files via upload

--- sdk-setup.sh
+++ sdk-setup.sh
@@ -8,9 +8,9 @@ sudo apt-get update
 sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
 sudo apt-get install docker-engine -y --allow-unauthenticated
 sudo apt-get install python-pip -y
-sudo apt-get install build-essential libxml2-dev libxslt1-dev zlib1g-dev -y  
-sudo wget python-dev PySocks-dev libffi-dev libssl-dev python-setuptools libjim-dev libdw-dev 
-sudo pip install kubos-sdk
+pip install --upgrade pip
+sudo apt-get install build-essential libxml2-dev libxslt1-dev zlib1g-dev wget python-dev libffi-dev libssl-dev python-setuptools libjim-dev libdw-dev -y 
+pip install kubos-sdk
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo kubos update
